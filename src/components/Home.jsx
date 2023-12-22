b8db16b9000c881990d005935946aedba36b33a6
johnmainakinyanjui <mychmk1@gmail.com>
2023-12-22 17:10:07 +0300

    nav and home space

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-scroll';
 
 export const Home = () => {
   return (
-    <div name="home" className=" pt-40 md:pt-0 h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+    <div name="home" className=" h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
 
         <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
 
