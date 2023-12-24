26d6c41493e0e5656c9314083a6dc4f25a56a0e3
johnmainakinyanjui <mychmk1@gmail.com>
2023-12-24 14:55:25 +0300

    about and portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -37,7 +37,7 @@ const Portfolio = () => {
 
 
   return (
-    <div name='portfolio' className=' bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
+    <div name='portfolio' className=' bg-gradient-to-b from-black to-gray-800 w-full text-white'>
       <div className='  max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full pb-20'>
         <div className='pb-4'>
           <p className=' text-4xl font-bold inline border-b-4 border-gray-500'>
