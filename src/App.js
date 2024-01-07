cc89dc02e126924da31e2419bd0f53655b6d19e4
johnmainakinyanjui <mychmk1@gmail.com>
2024-01-07 14:40:27 +0300

    footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { About } from "./components/About";
 import { Contact } from "./components/Contact";
 import { Counter } from "./components/Counter";
 import { Experience } from "./components/Experience";
+import { Footer } from "./components/Footer";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
 import Portfolio from "./components/Portfolio";
@@ -19,6 +20,7 @@ function App() {
       <Testimonials />
       <Counter />
       <Contact />
+      <Footer />
 
       <SocialLinks />
     </div>
